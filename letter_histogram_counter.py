f0aa321eb844de7d51334a341c96431c3dc6245c
maxwellmg <max@vale.network>
2022-05-06 16:56:50 -0400

    letter histogram nearly there

--- letter_histogram_counter.py
+++ letter_histogram_counter.py
@@ -1,21 +1,35 @@
 #word_list = ["butt", "tushie", "behind", "backside"]
-word_list = ["abcdefg", "aabbccddeeffgg", "aabccdeefgg"]
+#word_list = ["acdf"]
+word_list = ["abcdefg", "aabbccddeeffgg", "aabccdeefgg", "acdf"]
 #letter_histogram = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
 
 def letter_histogram_counter():
     #word_histogram = {}
     for word in word_list:
         letter_histogram = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0}
+        unique_letter_counter = 0
+        seven_unique_letter_words = []
         for letter in word:
             for key in letter_histogram:
                 if key == letter:
                     letter_histogram[key] = letter_histogram[key] + 1
                 else:
                     pass
-                #print(letter_histogram.value(letter))
             else:
                 pass
-        print(letter_histogram)
+        for key in letter_histogram:
+            #print("Key = " + str(key) + ", Value = " + str(letter_histogram[key]))
+            if letter_histogram[key] == 0:
+                pass
+            else:
+                unique_letter_counter +=1
+        if unique_letter_counter == 7:
+            seven_unique_letter_words.append(word)
+            print(seven_unique_letter_words)
+        else:
+            pass
+    print(seven_unique_letter_words)
+
 letter_histogram_counter()
 
 '''capitals = {"USA":"Washington D.C.", "France":"Paris", "India":"New Delhi"}
