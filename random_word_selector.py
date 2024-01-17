0a78b8c81250b1af0867a0aadbfee902878b0cf2
maxwellmg <max@vale.network>
2024-01-17 16:22:57 -0500

    code chooses random word and random letter in word

--- random_word_selector.py
+++ random_word_selector.py
@@ -1,1 +1,7 @@
-import 
+from random import choice
+
+list = ["abc", "def", "ghi"]
+chosen_panagram = choice(list)
+chosen_mandatory_letter = choice(chosen_panagram)
+print("The chosen word is " + chosen_panagram)
+print("The chosen letter is " + chosen_mandatory_letter)
