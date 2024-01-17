49fdb3253522185c8a61b26677033d8f21dd56be
maxwellmg <max@vale.network>
2024-01-17 16:57:44 -0500

    two functions for starting up game and printing

--- random_word_selector.py
+++ random_word_selector.py
@@ -1,7 +1,36 @@
 from random import choice
 
-list = ["abc", "def", "ghi"]
-chosen_panagram = choice(list)
-chosen_mandatory_letter = choice(chosen_panagram)
-print("The chosen word is " + chosen_panagram)
-print("The chosen letter is " + chosen_mandatory_letter)
+list = ["aabbccddeeffgg", "hhiijjkkllmmnn", "ooppqqrrssttuu"]
+
+def generate_game():
+    chosen_panagram = choice(list)
+    chosen_mandatory_letter = choice(chosen_panagram)
+    print("The chosen word is " + chosen_panagram)
+    print("The chosen letter is " + chosen_mandatory_letter)
+    return chosen_panagram, chosen_mandatory_letter
+#def randomize_letters():
+
+#def print_letters_in_terminal(chosen_panagram, chosen_mandatory_letter):
+def print_letters_in_terminal():
+    chosen_panagram = "hhiijjkkllmmnn"
+    chosen_mandatory_letter = "j"
+    unique_letters = []
+    all_but = chosen_panagram.replace(chosen_mandatory_letter, "")
+    #chosen_panagram(0)
+    for letter in all_but:
+        if letter in unique_letters:
+            pass
+        else:
+            unique_letters.append(letter)
+        print(unique_letters)
+    letter1 = unique_letters[0]
+    letter2 = unique_letters[1]
+    letter3 = unique_letters[2]
+    letter5 = unique_letters[3]
+    letter6 = unique_letters[4]
+    letter7 = unique_letters[5]
+    print("    " + letter1 + "     " + letter2 + "\n" + letter3 + "     [" + chosen_mandatory_letter + "]     " + letter5 + "\n    " + letter6 + "     " + letter7)
+
+#generate_game()
+#print_letters_in_terminal(generate_game())
+print_letters_in_terminal()
