a198d3504a9e957e22aca48c1e32016e00838165
maxwellmg <maxmillergolub@gmail.com>
2024-02-03 14:38:20 -0500

    ranking > ranking list, chosen_panagram removed

--- function_library.py
+++ function_library.py
@@ -8,7 +8,6 @@ from refining_panagrams.checked_panagrams_main import mwd_checked_panagrams
 def generate_game():
     chosen_panagram = choice(mwd_checked_panagrams)
     chosen_mandatory_letter = choice(chosen_panagram)
-    #print(chosen_panagram + "\n" + chosen_mandatory_letter)
     unique_letters = []
     all_but = chosen_panagram.replace(chosen_mandatory_letter, "")
     for letter in all_but:
@@ -17,13 +16,15 @@ def generate_game():
         else:
             unique_letters.append(letter)
     shuffle(unique_letters)
-    return [chosen_panagram, chosen_mandatory_letter, unique_letters]
+    return [chosen_mandatory_letter, unique_letters]
 
 # find_all_internal_words takes arguments of the chosen panagram and chosen mandatory letter and it finds and returns a list of all words that meet criteria
 
 def find_all_internal_words(variables):
-    chosen_panagram = variables[0]
-    chosen_mandatory_letter = variables[1]
+    chosen_mandatory_letter = variables[0]
+    unique_letters = variables[1]
+    all_letters = unique_letters
+    all_letters.append(chosen_mandatory_letter)
     good_words = []
     for word in dictionary:
         current_word = []
@@ -31,7 +32,7 @@ def find_all_internal_words(variables):
             continue
         else:
             for letter in word:
-                if letter not in chosen_panagram:
+                if letter not in all_letters:
                     break
                 else:
                     current_word.append(letter)
@@ -43,9 +44,9 @@ def find_all_internal_words(variables):
 
 # print_letters creates the print statement for the unique letters of the panagram
 
-def print_letters(variables):
-    chosen_mandatory_letter = variables[1]
-    unique_letters = variables[2]
+def print_letters(chosen_mandatory_letter, unique_letters):
+    #chosen_mandatory_letter = variables[1]
+    #unique_letters = variables[2]
     print("    " + unique_letters[0] + "     " + unique_letters[1] + "\n\n" + unique_letters[2] + "     [" + chosen_mandatory_letter + "]     " + unique_letters[3] + "\n\n    " + unique_letters[4] + "     " + unique_letters[5] + "\n")
 
 # points_system takes the argument of a new valid word and determines its value and corresponding print statement
@@ -70,16 +71,19 @@ def points_system(new_word):
 
 # guess_checker checks validity of the new inputted word from the user. it returns None if there is an error with the word (i.e. too short, already inputted, contains non-viable letters, etc.) or it sends the word to the points_system function if its a good word.
 
-def guess_checker(new_word, variables, words_found, good_words, score, highest_possible_score, ranking):
+def guess_checker(new_word, variables, words_found, good_words, score, highest_possible_score, ranking_variables):
+    #return [ranking, rank_marker_list, rankings]
+    #ranking = ranking_variables[0]
     new_word = new_word.upper()
-    chosen_panagram = variables[0]
-    chosen_mandatory_letter = variables[1]
-    unique_letters = variables[2]
+    #chosen_panagram = variables[0]
+    chosen_mandatory_letter = variables[0]
+    unique_letters = variables[1]
     if new_word.startswith("-") == True:
-        return help_menu(new_word, words_found, unique_letters, score, highest_possible_score, good_words, ranking)
+        return help_menu(new_word, words_found, unique_letters, score, highest_possible_score, good_words, ranking_variables)
     else:    
         for letter in new_word:
-            if letter not in chosen_panagram:
+            #if letter not in chosen_panagram:
+            if (letter not in unique_letters) and (letter != chosen_mandatory_letter):
                 print("\nContains non-viable letter (" + letter.upper() + ")\n")
                 return None
                 #break
@@ -102,30 +106,24 @@ def guess_checker(new_word, variables, words_found, good_words, score, highest_p
             else:
                 print("\n" + new_word + " not in word list\n")
 
-'''def menu(new_word):
-    user_input = ""
-    until user_input == "-e"
-        if (new_word == "-SHUFFLE") or (new_word == "-SHUFFLE"):
-            shuffle(unique_letters)
-            print("\n")
-        else:
-            print'''
-
-def help_menu(new_word, words_found, unique_letters, score, highest_possible_score, good_words, ranking):
+def help_menu(new_word, words_found, unique_letters, score, highest_possible_score, good_words, ranking_variables):
+    ranking = ranking_variables[0]
+    rank_marker_list = ranking_variables[1]
+    rankings = ranking_variables[2]
     beginning_input = new_word.lower()
     if (beginning_input == "-s") or (beginning_input == "-shuffle"):
         help_shuffle(unique_letters)
     elif (beginning_input == "-r") or (beginning_input == "-rules"):
         help_rules()
     elif (beginning_input == "-t") or (beginning_input == "-tiers"):
-        help_tiers()
+        help_tiers(rank_marker_list, rankings)
     elif (beginning_input == "-w") or (beginning_input == "-words_list"):
         help_words_list(words_found)
     elif (beginning_input == "-q") or (beginning_input == "-quit"):
         return quitting_prompt(words_found, score, highest_possible_score, good_words, ranking)
     elif (beginning_input == "-h") or (beginning_input == "-help"):
         while True:
-            beginning_input = input("\nHelp menu:\n\n-E: Exit Help Menu\n-S: Shuffle Letters (and Exit Help Menu)\n-R: See Rules\n-T: See Tier Rankings\n-W: See All Successful Word Attempts\n-Q: Quit Game\n\n")
+            beginning_input = input("Help menu:\n\n-E: Exit Help Menu\n-S: Shuffle Letters (and Exit Help Menu)\n-R: See Rules\n-T: See Tier Rankings\n-W: See All Successful Word Attempts\n-Q: Quit Game\n\n")
             if (beginning_input == "-e") or (beginning_input == "-exit") or (beginning_input == "e"):
                 print("\nExiting Help Menu\n")
                 time.sleep(1)
@@ -135,7 +133,7 @@ def help_menu(new_word, words_found, unique_letters, score, highest_possible_sco
             elif (beginning_input == "-r") or (beginning_input == "-rules") or (beginning_input == "r"):
                 help_rules()
             elif (beginning_input == "-t") or (beginning_input == "-tiers") or (beginning_input == "t"):
-                help_tiers()
+                help_tiers(rank_marker_list, rankings)
             elif (beginning_input == "-w") or (beginning_input == "-words_list") or (beginning_input == "w"):
                 help_words_list(words_found)
             elif (beginning_input == "-q") or (beginning_input == "-quit") or (beginning_input == "q"):
@@ -161,37 +159,43 @@ def ranking_finder(good_words):
 # ranking_assessor factors in the current user score with the highest possible score for the round to find consistent ranks. Rank based on current points as a percent of the total
 
 def ranking_assessor(highest_possible_score, current_score):
-    #establish percentages for ranking, have ranking titles
-    total = highest_possible_score
-    current = current_score
-    percent = 0
+    percents = [1.5, 4.5, 7.5, 14, 24, 38, 48, 68]
+    rank_marker_list = []
+    for number in percents:
+        rank_marker = round((number / 100) * highest_possible_score)
+        rank_marker_list.append(rank_marker)
+    
+    rankings = ["Born Yesterday", "Pre-K Reading Level", "Somebody knows their ABCs", "Mediocrity ain't half bad (jk)", "High School Reading Level", "I May be in Debt, but I am College Educated", "Congrats on your Master's Thesis, Dweeb", "How long have you been playing this game??", "JACK OF ALL TRADES, MASTER OF ALL TRADES. No More Words Left. What'd You Expect? A Cookie? Go Touch Grass."]
 
-    if current == 0:
+    if current_score == 0:
         ranking = "N/A"
     else:
-        percent = (current / total) * 100
-        if percent <= 1:
-            ranking = "Born Yesterday"
-        elif (percent > 1 and percent <= 3):
-            ranking = "Pre-K Reading Level"
-        elif percent > 3 and percent <= 7:
-            ranking = "Somebody knows their ABCs"
-        elif percent > 7 and percent <= 11:
-            ranking = "Mediocrity ain't half bad (jk)"
-        elif percent > 11 and percent <= 16:
-            ranking = "High School Reading Level"
-        elif percent > 16 and percent <= 22:
-            ranking = "I May be in Debt, but I am College Educated"
-        elif percent > 22 and percent < 100:
-            ranking = "How long have you been playing this game??"
-        else:
-            ranking = "JACK OF ALL TRADES, MASTER OF ALL TRADES. No More words go home."
-    return ranking
+        if current_score < rank_marker_list[0]:
+            ranking = rankings[0]
+        elif current_score >= rank_marker_list[0]:
+            ranking = rankings[1]
+        elif current_score >= rank_marker_list[1]:
+            ranking = rankings[2]
+        elif current_score >= rank_marker_list[2]:
+            ranking = rankings[3]
+        elif current_score >= rank_marker_list[3]:
+            ranking = rankings[4]
+        elif current_score >= rank_marker_list[4]:
+            ranking = rankings[5]
+        elif current_score >= rank_marker_list[5]:
+            ranking = rankings[6]
+        elif current_score >= rank_marker_list[6]:
+            ranking = rankings[7]
+        elif current_score >= rank_marker_list[7]:
+            ranking = rankings[8] 
+        elif current_score == highest_possible_score:
+            ranking = rankings[9]
+    return [ranking, rank_marker_list, rankings]
 
-# loading_menu_prompt picks a random print statement for the opening sequence to improve overall user experience
+# loading_prompt picks a random print statement for the opening sequence to improve overall user experience
 
-def loading_menu_prompt():
-    possible_statements = ['(Not) picking my nose...', 'Thinking about the meaning of life...', 'Am I getting hungry?...', 'Watching paint dry...', "What's that smell?...", 'Downloading malware (jk)...', 'Converting meters to inches...', 'Descaling the Keurig...', 'Fetching the newspaper...']
+def loading_prompt():
+    possible_statements = ['(Not) Picking my Nose...', 'Thinking About the Meaning of Life...', 'Am I Getting Hungry?...', 'Watching Paint Dry...', "What's That Smell?...", 'Downloading Malware (jk)...', 'Converting Meters to Inches...', 'Descaling the Keurig...', 'Fetching the Newspaper...']
     return(choice(possible_statements) + "\n")
 
 # print_recent_inputted_words outputs a short list of inputted words in reverse chronological order
@@ -259,9 +263,15 @@ def help_rules():
     print("Spelling Bee Rules\n\n1. There is at least 1 panagram per game\n2. Words must be at least 4 letters in length\n3. Words cannot be counted more than once\n4. Words must contain mandatory letter\n5. Point System:\n\t4-letters = 1pt\n\t5-letters = 5pt\n\t6-letters = 6pt\n\t...etc.\n\tPanagram = 7 additional points\n6. Player ranking determined by points gained vs. total points possible\n")
     time.sleep(1)
 
-def help_tiers():
-    print("\nTiers\n")
+def help_tiers(rank_marker_list, rankings):
+    print("\nScore Tiers\n")
     time.sleep(1)
+    count = 0
+    print("Score:  Ranking:\n")
+    for number in rank_marker_list:
+        print("  " + str(number) + "\t" + rankings[count])
+        count += 1
+    print('\n')
 
 def help_words_list(words_found):
     print("\nWords List\n")
